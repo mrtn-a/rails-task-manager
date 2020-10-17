86f4e224c24dde83e6a267c72452fa0ec8a8586a
Martyna Markiel <martyna.markiel@gmail.com>
2020-10-17 12:44:24 +0200

    added list all, list one and create task for the user

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index' # all
+  get 'tasks/new', to: 'tasks#new', as: :new_task # create new task
+  get 'tasks/:id', to: 'tasks#show', as: :task # 1 task
+  post 'tasks', to: 'tasks#create'
 end
